c696d31abc4cc44381b2f9ba717bd3be498937b6
Allan Cassio <allan.cassio1@gmail.com>
2024-01-18 10:21:35 -0300

    ...

---
+++ src/domain/useCases/AnswerQuestion.ts
@@ -0,0 +1,14 @@
+import { Answer } from '../entities/Answer'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content)
+    return answer
+  }
+}

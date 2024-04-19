471a67e4601fcb8e07efbd9d601c821ee3100a6a
Allan Cassio <allan.cassio1@gmail.com>
2024-04-19 05:48:18 -0300

    implement send notification to author on question when a new answer created

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepsitory } from '@/domain/forum/application/repositories/questions-repository'
 import { AnsewrCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../useCases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionsRepsitory,
+    private sendNotification: SendNotificationUseCase,
+  ) {
     this.setupSubscriptions()
   }
 
@@ -15,6 +20,15 @@ export class OnAnswerCreated implements EventHandler {
   }
 
   private async sendNewAnswerNotification({ answer }: AnsewrCreatedEvent) {
-    console.log('chamou')
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question)
+      await this.sendNotification.execute({
+        recipientId: question?.authorId.toString(),
+        title: `Nova resposta em "${question.title.substring(0, 40).concat('...')}"`,
+        content: answer.except,
+      })
   }
 }
